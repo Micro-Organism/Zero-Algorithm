c28a43feca78efc23a94dae0583825c1818a92be
organism <coolrabbit1993@gmail.com>
2024-04-12 14:13:02 +0800

    feat: 添加力扣第一个简单题目

---
+++ zero-leetcode/src/main/java/com/zero/algorithm/leetcode/Q1SumOfTwoNumber.java
@@ -0,0 +1,26 @@
+package com.zero.algorithm.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author rabbit
+ * @description 1.两数之和
+ * @createTime 2024/4/12 下午1:58
+ */
+public class Q1SumOfTwoNumber {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] res = new int[]{0,0};
+        for(int index = 0; index < nums.length; index++){
+            for(int number = index + 1; number < nums.length; number++ ){
+                if(nums[index]+nums[number] == target){
+                    res[0] = index;
+                    res[1] = number;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+}
---
+++ zero-leetcode/src/test/java/com/zero/algorithm/leetcode/Q1SumOfTwoNumberTest.java
@@ -0,0 +1,24 @@
+package com.zero.algorithm.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static com.zero.algorithm.leetcode.Q1SumOfTwoNumber.twoSum;
+
+/**
+ * @author rabbit
+ * @description 1.两数之和
+ * @createTime 2024/4/12 下午1:58
+ */
+public class Q1SumOfTwoNumberTest {
+
+    @Test
+    void test() {
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
